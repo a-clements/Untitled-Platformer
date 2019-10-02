5114b79c7053ebc067f1d3e49e331f9d00a141ff
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-10-02 13:06:49 +1000

    second map generation method complete

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    [SerializeField] private int Height;
-    [SerializeField] private int Width;
-    [SerializeField] private int Distance;
-    [SerializeField] private int SpacerMinimum;
-    [SerializeField] private int SpacerMaximum;
+    public bool MethodChange;
 
-    [SerializeField] private GameObject Dirt;
-    [SerializeField] private GameObject Grass;
-    [SerializeField] private GameObject Stone;
+    [Header("Prefabs")]
+    [SerializeField] private GameObject DirtPrefab;
+    [SerializeField] private GameObject GrassPrefab;
+    [SerializeField] private GameObject StonePrefab;
+    [SerializeField] private GameObject PlatformPrefab;
+    [SerializeField] private GameObject HazardPrefab;
+    [SerializeField] private GameObject BridgePrefab;
+
+    [Header("Common")]
+    [SerializeField] private int Distance;
+
+    [Header("Method 1 variables")]
+    [SerializeField] private int MethodOneHeight = 30;
+    [SerializeField] private int MethodOneWidth = 120;
+    [SerializeField] private int SpacerMinimum = 12;
+    [SerializeField] private int SpacerMaximum = 24;
 
     private int LowPoint;
     private int HighPoint;
     private int Spacer;
     private int TileSpace;
+
+    [Header("Method 2 variables")]
+    [SerializeField] private int MethodTwoHeight = 4;
+    [SerializeField] private int MethodTwoWidth = 120;
+    [SerializeField] private int MinPlatformSize = 1;
+    [SerializeField] private int MaxPlatformSize = 10;
+    [SerializeField] private int MaxHazards = 3;
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float ChanceofHazard = 0.5f;
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float ChanceofBridge = 0.1f;
+
+    private bool IsHazard;
+
+    [Header("Number of Platforms")]
+    [SerializeField] private int NumerOfPlatforms = 100;
+
 
     private int i;
 
@@ -29,27 +55,80 @@ public class Generator : MonoBehaviour
 
     void Generate()
     {
-        Distance = Height;
-
-        for(int w = 0; w <Width; w++)
+        if (MethodChange == false)
         {
-            LowPoint = Distance - 1;
-            HighPoint = Distance + 2;
-            Distance = Random.Range(LowPoint, HighPoint);
-            Spacer = Random.Range(SpacerMinimum, SpacerMaximum);
-            TileSpace = Distance - Spacer;
+            Distance = MethodOneHeight;
 
-            for(i = 0; i < TileSpace; i++)
+            for (int w = 0; w < MethodOneWidth; w++)
             {
-                Instantiate(Stone, new Vector3(w, i), Quaternion.identity);
+                LowPoint = Distance - 1;
+                HighPoint = Distance + 2;
+                Distance = Random.Range(LowPoint, HighPoint);
+                Spacer = Random.Range(SpacerMinimum, SpacerMaximum);
+                TileSpace = Distance - Spacer;
+
+                for (i = 0; i < TileSpace; i++)
+                {
+                    Instantiate(StonePrefab, new Vector3(w, i), Quaternion.identity);
+                }
+
+                for (i = TileSpace; i < Distance; i++)
+                {
+                    Instantiate(DirtPrefab, new Vector3(w, i), Quaternion.identity);
+                }
+
+                Instantiate(GrassPrefab, new Vector3(w, Distance), Quaternion.identity);
+
+                PlacePlatform(Random.Range(0,2),  w, Random.Range(Distance + 3, Distance + 5));
+            }
+        }
+
+        else
+        {
+            for (int w = 0; w < MethodTwoWidth; w++)
+            {
+                if(Random.value < ChanceofBridge)
+                {
+                    Instantiate(BridgePrefab, new Vector3(w, 0),Quaternion.identity);
+                }
+
+                else if(Random.value < ChanceofHazard)
+                {
+                    Instantiate(StonePrefab, new Vector3(w, -2), Quaternion.identity);
+                    Instantiate(DirtPrefab, new Vector3(w, -1), Quaternion.identity);
+                    Instantiate(HazardPrefab, new Vector3(w, 0), Quaternion.identity);
+                }
+
+                else
+                {
+                    Instantiate(StonePrefab, new Vector3(w, -2), Quaternion.identity);
+                    Instantiate(DirtPrefab, new Vector3(w, -1), Quaternion.identity);
+                    Instantiate(GrassPrefab, new Vector3(w, 0), Quaternion.identity);
+                }
+
+                PlacePlatform(Random.Range(0, 2), w, Random.Range(2, MethodTwoHeight + 1));
+            }
+        }
+    }
+
+    void PlacePlatform(int value, int w, int h)
+    {
+        if(value < 1)
+        {
+            if (Random.value < ChanceofHazard)
+            {
+                GameObject Platform = Instantiate(HazardPrefab, new Vector3(w, h), Quaternion.identity) as GameObject;
             }
 
-            for(i = TileSpace; i < Distance; i++)
+            else if (Random.value < ChanceofBridge)
             {
-                Instantiate(Dirt, new Vector3(w, i), Quaternion.identity);
+                GameObject Platform = Instantiate(BridgePrefab, new Vector3(w, h), Quaternion.identity) as GameObject;
             }
 
-            Instantiate(Grass, new Vector3(w, Distance), Quaternion.identity);
+            else
+            {
+                GameObject Platform = Instantiate(PlatformPrefab, new Vector3(w, h), Quaternion.identity) as GameObject;
+            }
         }
     }
 }
