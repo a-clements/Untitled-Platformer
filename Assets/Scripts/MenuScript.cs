06a6802e6ff7b627f214aaa33589f0080e431ddc
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-10-13 12:39:11 +1100

    Menu panels now work backwards to clear the screen after a button has been clicked

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour
 {
+    [Header("Game Objects")]
     public GameObject LeftPanel;
     public GameObject RightPanel;
     public GameObject BottomPanel;
+
+    [Header("Panel Speed")]
     [SerializeField] private float SidePanelSpeed = 1.0f;
     [SerializeField] private float BottomPanelSpeed = 1.0f;
-    [SerializeField] private float SidePanelMultiplier = 1.5f;
-    [SerializeField] private float BottomPanelMultipler = 1.5f;
+
+    [Header("Panel Multiplier")]
+    [SerializeField] private float SidePanelSlideInMultiplier = 1.5f;
+    [SerializeField] private float BottomPanelSlideInMultiplier = 1.5f;
+    [SerializeField] private float SidePanelSlideOutMultiplier = 2.5f;
+    [SerializeField] private float BottomPanelSlideOutMultiplier = 2.5f;
+
+    [Header("Coroutine Timers")]
     [SerializeField] private float WaitTimer = 0.01f;
 
     void Start()
@@ -18,44 +27,32 @@ public class MenuScript : MonoBehaviour
         StartCoroutine(ScrollIn());
     }
 
+    #region Panel Enumerators do NOT touch
     private IEnumerator ScrollIn()
     {
-        StartCoroutine(LeftPanelScrollIn());
-        StartCoroutine(LeftRightScrollIn());
+        StartCoroutine(SidePanelScrollIn());
         StartCoroutine(BottomPanelScrollIn());
         yield return null;
     }
 
-    IEnumerator LeftPanelScrollIn()
+    IEnumerator SidePanelScrollIn()
     {
         float n;
+        float i;
 
         yield return new WaitForSeconds(WaitTimer);
 
         n = LeftPanel.GetComponent<RectTransform>().localPosition.x;
-        while (LeftPanel.GetComponent<RectTransform>().localPosition.x < -(LeftPanel.GetComponent<RectTransform>().rect.width * SidePanelMultiplier))
+        i = RightPanel.GetComponent<RectTransform>().localPosition.x;
+
+        while (LeftPanel.GetComponent<RectTransform>().localPosition.x < -(LeftPanel.GetComponent<RectTransform>().rect.width * SidePanelSlideInMultiplier) && 
+            RightPanel.GetComponent<RectTransform>().localPosition.x > (RightPanel.GetComponent<RectTransform>().rect.width * SidePanelSlideInMultiplier))
         {
             LeftPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(n, 0);
             n = n + SidePanelSpeed;
 
-            yield return new WaitForEndOfFrame();
-        }
-
-        yield return null;
-    }
-
-    IEnumerator LeftRightScrollIn()
-    {
-        float n;
-
-        yield return new WaitForSeconds(WaitTimer);
-
-        n = RightPanel.GetComponent<RectTransform>().localPosition.x;
-
-        while (RightPanel.GetComponent<RectTransform>().localPosition.x > (RightPanel.GetComponent<RectTransform>().rect.width * SidePanelMultiplier))
-        {
-            RightPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(n, 0);
-            n = n - SidePanelSpeed;
+            RightPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(i, 0);
+            i = i - SidePanelSpeed;
 
             yield return new WaitForEndOfFrame();
         }
@@ -71,7 +68,7 @@ public class MenuScript : MonoBehaviour
 
         n = BottomPanel.GetComponent<RectTransform>().localPosition.y;
 
-        while (BottomPanel.GetComponent<RectTransform>().localPosition.y < -(BottomPanel.GetComponent<RectTransform>().rect.height * SidePanelMultiplier))
+        while (BottomPanel.GetComponent<RectTransform>().localPosition.y < -(BottomPanel.GetComponent<RectTransform>().rect.height * BottomPanelSlideInMultiplier))
         {
             BottomPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, n);
             n = n + BottomPanelSpeed;
@@ -84,6 +81,55 @@ public class MenuScript : MonoBehaviour
 
     private IEnumerator ScrollOut()
     {
+        StartCoroutine(SidePanelScrollOut());
+        StartCoroutine(BottomPanelScrollOut());
         yield return null;
     }
+
+    IEnumerator SidePanelScrollOut()
+    {
+        float n;
+        float i;
+
+        yield return new WaitForSeconds(WaitTimer);
+
+        n = LeftPanel.GetComponent<RectTransform>().localPosition.x;
+        i = RightPanel.GetComponent<RectTransform>().localPosition.x;
+
+        while (LeftPanel.GetComponent<RectTransform>().localPosition.x > -(LeftPanel.GetComponent<RectTransform>().rect.width * SidePanelSlideOutMultiplier) &&
+            RightPanel.GetComponent<RectTransform>().localPosition.x < (RightPanel.GetComponent<RectTransform>().rect.width * SidePanelSlideOutMultiplier))
+        {
+            LeftPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(n, 0);
+            n = n - SidePanelSpeed;
+
+            RightPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(i, 0);
+            i = i + SidePanelSpeed;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return null;
+    }
+
+    IEnumerator BottomPanelScrollOut()
+    {
+        float n;
+
+        yield return new WaitForSeconds(WaitTimer);
+
+        n = BottomPanel.GetComponent<RectTransform>().localPosition.y;
+
+        Debug.Log(-(BottomPanel.GetComponent<RectTransform>().rect.height * BottomPanelSlideOutMultiplier));
+
+        while (BottomPanel.GetComponent<RectTransform>().localPosition.y > -(BottomPanel.GetComponent<RectTransform>().rect.height * BottomPanelSlideOutMultiplier))
+        {
+            BottomPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, n);
+            n = n - BottomPanelSpeed;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return null;
+    }
+    #endregion
 }
