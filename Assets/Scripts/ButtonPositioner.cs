7e6a7c190108b7d2b4d324cd0f70c32bb2371190
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-10-16 18:48:01 +1100

    Create ButtonPositioner.cs

---
+++ Assets/Scripts/ButtonPositioner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ButtonPositioner : MonoBehaviour
+{
+    [SerializeField] private Button[] Buttons;
+    [SerializeField] private float ButtonHeight;
+    [SerializeField] private float ButtonWidth;
+    [SerializeField] private float Position;
+
+    public void Positioner()
+    {
+        Array.Resize(ref Buttons, this.transform.childCount);
+
+        ButtonHeight = this.GetComponent<RectTransform>().rect.height / (Buttons.Length + 1);
+
+        ButtonWidth = this.GetComponent<RectTransform>().rect.width * 0.5f;
+
+        Position = ButtonHeight * Buttons.Length;
+
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Buttons[i] = this.transform.GetChild(i).GetComponent<Button>();
+            Buttons[i].GetComponent<RectTransform>().sizeDelta = new Vector2(ButtonWidth, ButtonHeight);
+            Buttons[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(0, ButtonHeight - Position);
+            Position -= ButtonHeight;
+        }
+    }
+}
