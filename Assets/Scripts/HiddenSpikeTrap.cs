7ae9d51f5358006f3c75334fa04d3cb7eaa56cc3
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-10-21 18:31:16 +1100

    basic movement
    player prefab created, fixed a bug with hidden traps

--- Assets/Scripts/HiddenSpikeTrap.cs
+++ Assets/Scripts/HiddenSpikeTrap.cs
@@ -6,6 +6,11 @@ public class HiddenSpikeTrap : MonoBehaviour
 {
     [SerializeField] private GameObject SpikeTrap;
 
+    private void OnEnable()
+    {
+        SpikeTrap = this.transform.GetChild(0).gameObject;
+    }
+
     private void OnCollisionEnter2D(Collision2D CollisionInfo)
     {
         if(CollisionInfo.transform.tag == "Player")
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private GameManager Manager;
+    [SerializeField] private float JumpHeight;
+    [SerializeField] private float RunSpeed;
+    private Rigidbody2D RigidBody;
+    private Transform ThisTransform;
+
+    private void Awake()
+    {
+        Manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    private void OnEnable()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+        ThisTransform = this.transform;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(Manager.Keys[0]))
+        {
+            ThisTransform.Translate(Vector2.left * Time.deltaTime * RunSpeed, Space.Self);
+        }
+
+        if (Input.GetKey(Manager.Keys[1]))
+        {
+            ThisTransform.Translate(Vector2.right * Time.deltaTime * RunSpeed, Space.Self);
+        }
+
+        if (Input.GetKeyDown(Manager.Keys[2]))
+        {
+
+        }
+
+        if (Input.GetKeyDown(Manager.Keys[3]))
+        {
+
+        }
+
+        if (Input.GetKeyDown(Manager.Keys[4]))
+        {
+
+        }
+
+        if (Input.GetKeyDown(Manager.Keys[5]))
+        {
+            RigidBody.velocity = Vector2.up * Time.deltaTime * JumpHeight;
+        }
+    }
+}
