838d3140b4b236cf49bc2a83e32fbb8f7cea83bf
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-10-27 10:17:11 +1100

    Update SetTileColour.cs

--- Assets/Scripts/SetTileColour.cs
+++ Assets/Scripts/SetTileColour.cs
@@ -6,11 +6,11 @@ public class SetTileColour : MonoBehaviour
 {
     private MaterialPropertyBlock PropertyBlock;
 
-    [Range(-100,100)]
-    public float saturation = 0.0f;
+    //[Range(-100,100)]
+    //public float saturation = 0.0f;
 
-    [Range(1, 10)]
-    public float contrast = 1.0f;
+    //[Range(1, 10)]
+    //public float contrast = 1.0f;
 
     [Range(-1, 0)]
     public float exposure = 0.0f;
@@ -18,16 +18,52 @@ public class SetTileColour : MonoBehaviour
     void Start()
     {
         PropertyBlock = new MaterialPropertyBlock();
-        exposure = -0.5f;
+        
+        if(transform.childCount > 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                this.transform.GetChild(i).GetComponent<Renderer>().GetPropertyBlock(PropertyBlock);
+                //PropertyBlock.SetFloat("_Saturation", saturation);
+                //PropertyBlock.SetFloat("_Contrast", contrast);
+                exposure = -1.0f;
+                PropertyBlock.SetFloat("_Exposure", exposure);
+                this.transform.GetChild(i).GetComponent<Renderer>().SetPropertyBlock(PropertyBlock);
+            }
+        }
+
+        else
+        {
+            this.GetComponent<Renderer>().GetPropertyBlock(PropertyBlock);
+            //PropertyBlock.SetFloat("_Saturation", saturation);
+            //PropertyBlock.SetFloat("_Contrast", contrast);
+            exposure = -1.0f;
+            PropertyBlock.SetFloat("_Exposure", exposure);
+            this.GetComponent<Renderer>().SetPropertyBlock(PropertyBlock);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D CollisionInfo)
     {
         this.GetComponent<Renderer>().GetPropertyBlock(PropertyBlock);
-        PropertyBlock.SetFloat("_Saturation", saturation);
-        PropertyBlock.SetFloat("_Contrast", contrast);
+        //PropertyBlock.SetFloat("_Saturation", saturation);
+        //PropertyBlock.SetFloat("_Contrast", contrast);
+        exposure = 0.0f;
         PropertyBlock.SetFloat("_Exposure", exposure);
         this.GetComponent<Renderer>().SetPropertyBlock(PropertyBlock);
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    for (int i = 0; i < transform.childCount; i++)
+    //    {
+    //        this.transform.GetChild(i).GetComponent<Renderer>().GetPropertyBlock(PropertyBlock);
+    //        //PropertyBlock.SetFloat("_Saturation", saturation);
+    //        //PropertyBlock.SetFloat("_Contrast", contrast);
+    //        exposure = 0.0f;
+    //        PropertyBlock.SetFloat("_Exposure", exposure);
+    //        this.transform.GetChild(i).GetComponent<Renderer>().SetPropertyBlock(PropertyBlock);
+    //    }
+    //}
 }
