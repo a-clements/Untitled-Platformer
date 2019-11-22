5f5cb5fef52b378dd2395bdc8dcabfdf0f349a9b
Antony <antony.clements@bigpond.com>
2019-11-22 17:46:08 +1100

    modified a few scripts and prefabs

--- Assets/Scripts/AntiWallClimb.cs
+++ Assets/Scripts/AntiWallClimb.cs
@@ -13,35 +13,38 @@ public class AntiWallClimb : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D CollisionInfo)
     {
-        foreach (ContactPoint2D point2D in CollisionInfo.contacts)
+        if(CollisionInfo.transform.tag == "Player")
         {
-            if (point2D.normal.y <= 0)
+            foreach (ContactPoint2D point2D in CollisionInfo.contacts)
             {
-                CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = true;
-                CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = 1;
-            }
+                if (point2D.normal.y <= 0)
+                {
+                    CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = true;
+                    CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = 1;
+                }
 
-            else
-            {
-                CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = false;
-            }
+                else
+                {
+                    CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = false;
+                }
 
-            if (point2D.normal.x >= 0)
-            {
-                CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = false;
-                CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = -1;
-                CollisionInfo.transform.position = new Vector2(CollisionInfo.transform.position.x + 0.0125f, CollisionInfo.transform.position.y);
-                CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = true;
-                CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = 1;
-            }
+                if (point2D.normal.x >= 0)
+                {
+                    CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = false;
+                    CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = -1;
+                    CollisionInfo.transform.position = new Vector2(CollisionInfo.transform.position.x + 0.0125f, CollisionInfo.transform.position.y);
+                    CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = true;
+                    CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = 1;
+                }
 
-            if (point2D.normal.x <= 0)
-            {
-                CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = false;
-                CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = -1;
-                CollisionInfo.transform.position = new Vector2(CollisionInfo.transform.position.x - 0.0125f, CollisionInfo.transform.position.y);
-                CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = true;
-                CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = 1;
+                if (point2D.normal.x <= 0)
+                {
+                    CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = false;
+                    CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = -1;
+                    CollisionInfo.transform.position = new Vector2(CollisionInfo.transform.position.x - 0.0125f, CollisionInfo.transform.position.y);
+                    CollisionInfo.transform.GetComponent<PlayerMove>().CanJump = true;
+                    CollisionInfo.transform.GetComponent<PlayerMove>().JumpCount = 1;
+                }
             }
         }
     }
