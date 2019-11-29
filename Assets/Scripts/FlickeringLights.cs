c51ea7c5099bd54c346ec78483e73a7ff076c3d8
Antony <antony.clements@bigpond.com>
2019-11-29 17:46:17 +1100

    replaced hard values with variables

--- Assets/Scripts/FlickeringLights.cs
+++ Assets/Scripts/FlickeringLights.cs
@@ -7,6 +7,12 @@ public class FlickeringLights : MonoBehaviour
 {
     public bool LightsActive = false;
     [SerializeField] private float FlickerTimer;
+    [SerializeField] private int MinOuterRadius = 1;
+    [SerializeField] private int MaxOuterRadius = 5;
+    [SerializeField] private int MinColourRange = 60;
+    [SerializeField] private int MaxColourRange = 121;
+    [SerializeField] private int MinAlpha = 128;
+    [SerializeField] private int MaxAlpha = 256;
 
     void Start()
     {
@@ -18,8 +24,8 @@ public class FlickeringLights : MonoBehaviour
         while (LightsActive == true)
         {
             FlickerTimer = Random.Range(0.01f, 0.05f);
-            GetComponent<Light2D>().pointLightOuterRadius = Random.Range(1, 5);
-            GetComponent<Light2D>().color = new Color32(255,(byte)Random.Range(60,121),0,(byte)Random.Range(128,256));
+            GetComponent<Light2D>().pointLightOuterRadius = Random.Range(MinOuterRadius, MaxOuterRadius);
+            GetComponent<Light2D>().color = new Color32(255,(byte)Random.Range(MinColourRange,MaxColourRange),0,(byte)Random.Range(MinAlpha,MaxAlpha));
             GetComponent<Light2D>().intensity = Random.Range(0.5f,2.0f);
             yield return new WaitForSeconds(FlickerTimer);
         }
