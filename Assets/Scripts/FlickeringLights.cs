34686b334492b84e1df215d7e08c156127a1c92d
Antony <antony.clements@bigpond.com>
2019-12-01 12:33:51 +1100

    changed a few materials and gave the designer more control over the lights

--- Assets/Scripts/FlickeringLights.cs
+++ Assets/Scripts/FlickeringLights.cs
@@ -7,10 +7,14 @@ public class FlickeringLights : MonoBehaviour
 {
     public bool LightsActive = false;
     [SerializeField] private float FlickerTimer;
+    [SerializeField] private float MinFlickerTime = 0.01f;
+    [SerializeField] private float MaxFlickerTime = 0.05f;
     [SerializeField] private int MinOuterRadius = 1;
     [SerializeField] private int MaxOuterRadius = 5;
     [SerializeField] private int MinColourRange = 60;
     [SerializeField] private int MaxColourRange = 121;
+    [SerializeField] private float MinIntensity = 0.5f;
+    [SerializeField] private float MaxIntensity = 2.0f;
     [SerializeField] private int MinAlpha = 128;
     [SerializeField] private int MaxAlpha = 256;
 
@@ -23,10 +27,10 @@ public class FlickeringLights : MonoBehaviour
     {
         while (LightsActive == true)
         {
-            FlickerTimer = Random.Range(0.01f, 0.05f);
+            FlickerTimer = Random.Range(MinFlickerTime, MaxFlickerTime);
             GetComponent<Light2D>().pointLightOuterRadius = Random.Range(MinOuterRadius, MaxOuterRadius);
             GetComponent<Light2D>().color = new Color32(255,(byte)Random.Range(MinColourRange,MaxColourRange),0,(byte)Random.Range(MinAlpha,MaxAlpha));
-            GetComponent<Light2D>().intensity = Random.Range(0.5f,2.0f);
+            GetComponent<Light2D>().intensity = Random.Range(MinIntensity, MaxIntensity);
             yield return new WaitForSeconds(FlickerTimer);
         }
 
