eecf1de57ffc9ee909c4caf0aff8669f11699e1a
Antony <antony.clements@bigpond.com>
2020-01-19 18:45:53 +1100

    updated the platform and enemy scripts

--- Assets/Scripts/FlyingEnemyMove.cs
+++ Assets/Scripts/FlyingEnemyMove.cs
@@ -11,118 +11,67 @@ public class FlyingEnemyMove : MonoBehaviour
     /// 
 
     private Transform ThisTransform;
-    private Rigidbody RigidBody;
+    private Rigidbody2D RigidBody;
     private SpriteRenderer Sprite;
     private FadeOut Fade;
     private AudioSource SFX;
+    private Vector3 NextPosition;
+    private int PointNumber = 0;
 
-    [Tooltip("The sprite to show upon death of the flyer goes here.")]
-    [SerializeField] private Sprite DeathSprite;
-    [Tooltip("A declaration of the time frame over which the flyer will fade after death.")]
-    [SerializeField] private float FadeOutTime = 1.0f;
     [Tooltip("A declaration of how many points will be added to the score when the flyer dies.")]
     [SerializeField] private int PointValue;
-    [Tooltip("A decleration of which axis the flyer will move on.")]
-    [SerializeField] private string Direction;
+    [Tooltip("A declaration of the time frame over which the flyer will fade after death.")]
+    [SerializeField] private float FadeOutTime = 1.0f;
+    [Tooltip("The sprite to show upon death of the flyer goes here.")]
+    [SerializeField] private Sprite DeathSprite;
+    [Tooltip("A boolean to show if the enemy is moving or not.")]
+    [SerializeField] private bool IsMoving;
     [Tooltip("A declaration of how fast the flyer is moving. Only positive numbers.")]
     [SerializeField] private float Speed;
-    [Tooltip("A declaration of whether the flyer is moving between point to point on the X axis, rather than by collision.")]
-    [SerializeField] private bool XAxis;
-    [Tooltip("A declaration of the maximum position of the flyer while from point to point on the X axis")]
-    [SerializeField] private float MaxXDistance = 0.0f;
-    [Tooltip("A declaration of the minimum position of the flyer while from point to point on the X axis")]
-    [SerializeField] private float MinXDistance = 0.0f;
-    [Tooltip("A declaration of whether the flyer is moving between point to point on the Y axis, rather than by collision.")]
-    [SerializeField] private bool YAxis;
-    [Tooltip("A declaration of the maximum position of the flyer while from point to point on the Y axis")]
-    [SerializeField] private float MaxYDistance = 0.0f;
-    [Tooltip("A declaration of the minimum position of the flyer while from point to point on the Y axis")]
-    [SerializeField] private float MinYDistance = 0.0f;
-    [Tooltip("Is the flyer dead. This variable does not get used by the designer but needs to be public for other scripts to access it.")]
+    [Tooltip("An array of points at which the enemy will stop.")]
+    [SerializeField] private Vector3[] Points;
     public bool Dead;
-    [SerializeField] private bool CanReverse;
 
     void Start()
     {
         ThisTransform = transform;
-        RigidBody = GetComponent<Rigidbody>();
+        RigidBody = GetComponent<Rigidbody2D>();
         Sprite = GetComponent<SpriteRenderer>();
-        Fade = GetComponent<FadeOut>();
-        SFX = GetComponent<AudioSource>();
 
-        SFX.loop = true;
-
-        SFX.Play();
+        if (IsMoving == true)
+        {
+            NextPosition = Points[PointNumber];
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D CollisionInfo)
     {
-        if (CollisionInfo.transform.tag == "Rock")
+        if (CollisionInfo.transform.tag == "Ground")
         {
-            Dead = true;
-        }
-
-        if (CollisionInfo.transform.tag == "Ground" || CollisionInfo.transform.tag == "Enemy")
-        {
-            Speed *= -1.0f;
+            //Reverse = !Reverse;
         }
     }
 
-    private void FixedUpdate()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Time.timeScale == 0)
+        if (IsMoving == true)
         {
-            SFX.Pause();
-        }
-        else
-        {
-            SFX.UnPause();
-        }
-
-        if (XAxis == true)
-        {
-            if (ThisTransform.localPosition.x >= MaxXDistance && CanReverse == true || ThisTransform.localPosition.x <= MinXDistance && CanReverse == true)
+            if (ThisTransform.position == Points[PointNumber])
             {
-                Speed *= -1.0f;
+                PointNumber++;
+
+                if (PointNumber == Points.Length)
+                {
+                    PointNumber = 0;
+                }
+
+                NextPosition = Points[PointNumber];
+
                 Sprite.flipX = !Sprite.flipX;
-                CanReverse = false;
             }
 
-            if (ThisTransform.localPosition.x < MaxXDistance || ThisTransform.localPosition.x > MinXDistance)
-            {
-                CanReverse = true;
-            }
-        }
-
-        if (YAxis == true)
-        {
-            if (ThisTransform.localPosition.y >= MaxYDistance && CanReverse == true || ThisTransform.localPosition.y <= MinYDistance && CanReverse == true)
-            {
-                Speed *= -1.0f;
-                CanReverse = false;
-            }
-
-            if (ThisTransform.localPosition.y < MaxYDistance || ThisTransform.localPosition.y > MinYDistance)
-            {
-                CanReverse = true;
-            }
-        }
-
-        switch (Direction)
-        {
-            case "Up":
-                ThisTransform.Translate(Vector2.down * Speed * Time.deltaTime);
-                break;
-            case "Left":
-                ThisTransform.Translate(Vector2.left * Speed * Time.deltaTime);
-                break;
-        }
+            ThisTransform.position = Vector3.MoveTowards(ThisTransform.position, NextPosition, Speed);
+        }  
 
         if (Dead == true)
         {
