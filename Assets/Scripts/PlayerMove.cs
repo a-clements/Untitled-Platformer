ba0ce5023bd764e898582a417cbd06ce12bcd003
Antony <antony.clements@bigpond.com>
2020-01-22 11:20:45 +1100

    bug fix and started training tool tips

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -12,6 +12,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float FallModifier = 1.5f;
     public Animator PlayerAnimator;
     private Rigidbody2D RigidBody;
+    private BoxCollider2D BoxCollider;
     private Transform ThisTransform;
     public int JumpCount = 1;
     public bool CanJump = true;
@@ -27,6 +28,7 @@ public class PlayerMove : MonoBehaviour
     private void OnEnable()
     {
         RigidBody = GetComponent<Rigidbody2D>();
+        BoxCollider = GetComponent<BoxCollider2D>();
         ThisTransform = this.transform;
     }
 
@@ -81,6 +83,23 @@ public class PlayerMove : MonoBehaviour
                 PlayerAnimator.SetBool("IsWalking", false);
             }
             #endregion
+
+            if(PlayerAnimator.GetBool("IsWalking") == true || PlayerAnimator.GetBool("IsJumping") == true)
+            {
+                BoxCollider.offset = new Vector2(0.0f, -.07f);
+                BoxCollider.size = new Vector2(0.5f, 1.0f);
+            }
+            
+            else if(PlayerAnimator.GetBool("IsIdle") == true)
+            {
+                BoxCollider.offset = new Vector2(0, -.32f);
+                BoxCollider.size = new Vector2(0.5f, 0.5f);
+            }
+
+            else
+            {
+
+            }
         #endregion
 
         #region Jump
