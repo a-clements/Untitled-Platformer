a88819c4ff6050726f0c7cbeaca884135661a2b8
Antony <antony.clements@bigpond.com>
2020-02-11 09:10:00 +1100

    deleted the opening credits scene

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -21,9 +21,6 @@ public class GameSettings
     public string Fireupkey;
     public string Throwkey;
     public string Jumpkey;
-    public string Fireupkey;
-    public string Throwkey;
-    public string Jumpkey;
     public float Exposure;
     public int Hue;
     public int Contrast;
