ac31eba4f98fecc37dbe4f519392c2b287156798
Antony <antony.clements@bigpond.com>
2020-02-25 19:35:49 +1100

    level randomiser script and prefab done

---
+++ Assets/Scripts/LevelRandomiser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRandomiser : MonoBehaviour
+{
+    List<GameObject> ModulesList = new List<GameObject>();
+
+    [SerializeField] private GameObject[] Modules;
+
+    private int ModulePosition = 0;
+    private int j;
+
+    private void OnEnable()
+    {
+        for(int i = 0; i < Modules.Length; i++)
+        {
+            ModulesList.Add(Modules[i]);
+        }
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < Modules.Length; i++)
+        {
+            ModulePosition += 24;
+            j = Random.Range(0, ModulesList.Count);
+            ModulesList[j].transform.position = new Vector2(ModulePosition, ModulesList[j].transform.position.y);
+            ModulesList.RemoveAt(j);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
