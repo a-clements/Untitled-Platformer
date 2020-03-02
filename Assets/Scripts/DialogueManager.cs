334b520d5f2f2f511fc714384cf49fb490b3ec17
Antony <antony.clements@bigpond.com>
2020-03-02 20:03:50 +1100

    cut scene

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> Dialogue = new Queue<string>();
+    // Start is called before the first frame update
+
+    public static DialogueManager Instance = null;
+
+    [SerializeField] private Text DialogueText;
+    [SerializeField] private string SceneName;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Dialogue.Clear();
+
+        foreach(string Sentence in dialogue.Sentences)
+        {
+            Dialogue.Enqueue(Sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(Dialogue.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(Typing(Dialogue.Dequeue()));
+        }
+    }
+
+    void EndDialogue()
+    {
+        SceneManager.LoadSceneAsync(SceneName, LoadSceneMode.Single);
+    }
+
+    IEnumerator Typing(string dialogue)
+    {
+        DialogueText.text = "";
+
+        yield return new WaitForFixedUpdate();
+
+        foreach (char letter in dialogue.ToCharArray())
+        {
+            DialogueText.text += letter;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield return null;
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            DisplayNextSentence();
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue GetDialogue;
+
+    void TriggerDialogue()
+    {
+        DialogueManager.Instance.StartDialogue(GetDialogue);
+    }
+
+    private void Start()
+    {
+        TriggerDialogue();
+    }
+}
