ed3f95bcd6a63c047ba5c3c86c14cd43fea6a077
Antony <antony.clements@bigpond.com>
2020-03-06 13:15:58 +1100

    bug fix

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour
 {
+    [SerializeField] private string MapOne;
+    [SerializeField] private string MapTwo;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void ZoneOne()
+    {
+        SceneManager.LoadSceneAsync(MapOne, LoadSceneMode.Single);
+    }
+
+    public void ZoneTwo()
+    {
+        SceneManager.LoadSceneAsync(MapTwo, LoadSceneMode.Single);
     }
 
     // Update is called once per frame
