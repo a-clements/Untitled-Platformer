6a76abc4244a64ff22ed1d6d90804e682f38167d
Antony <antony.clements@bigpond.com>
2020-04-07 10:45:27 +1000

    main menu is now parallax

---
+++ Assets/Scripts/ParallaxSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxSpeed : MonoBehaviour
+{
+    [SerializeField] private float Parallaxspeed;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        this.GetComponent<RawImage>().uvRect = new Rect(Time.time * Parallaxspeed, 0, 1, 1);
+    }
+}
